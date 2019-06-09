3f9289e98ac98e2979a992ed4cd4f659166f69e8
Ali <alisalmani200149@gmail.com>
2019-06-09 21:24:55 +0430

    Added django-jet for admin panel.

--- chegg-backend/chegg/settings.py
+++ chegg-backend/chegg/settings.py
@@ -30,6 +30,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- chegg-backend/chegg/urls.py
+++ chegg-backend/chegg/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls import static, url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('store/', include('store.urls'))
