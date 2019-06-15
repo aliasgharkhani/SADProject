519430289978c44092228404e62c9b16b14fab26
Ali <alisalmani200149@gmail.com>
2019-06-15 11:47:05 +0430

    modals + UI to back

--- chegg-backend/store/admin.py
+++ chegg-backend/store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book, Problem, Chapter
+from .models import Book, Problem, Chapter, PurchaseHistory
 
 admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(Problem)
+admin.site.register(PurchaseHistory)
