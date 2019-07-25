7281011404cd215c006bc97dc221708fe4b9860a
Ali <alisalmani200149@gmail.com>
2019-07-25 09:32:18 +0430

    bugfix

--- chegg-backend/QA/models.py
+++ chegg-backend/QA/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     score = models.IntegerField(default=0)
     title = models.CharField(max_length=60)
-    description = models.CharField(max_length=1000)
+    body = models.CharField(max_length=1000)
     creator = models.ForeignKey('authentication.Member', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True, null=True)
 
