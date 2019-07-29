5618e020b0110d3575d9f8980357de0ffd0cebdf
Ali <alisalmani200149@gmail.com>
2019-07-29 14:01:24 +0430

    search questions by tag name

--- chegg-backend/QA/views.py
+++ chegg-backend/QA/views.py
@@ -16,6 +16,9 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
 
     def get_queryset(self):
+        if self.request.GET.get('tag', None):
+            tag_name = self.request.GET['tag']
+            return Question.objects.filter(tags__name=tag_name).distinct().order_by('-id')
         return Question.objects.all().order_by('-id')
 
     def get_serializer_context(self):
